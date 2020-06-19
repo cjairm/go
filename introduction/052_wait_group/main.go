4b268592b96cd66784c1aca27b6ff7f0b4fa104b
Carlos Mendez <carlos@hadaelectronics.com>
2020-06-18 19:54:10 -0500

    wait group

---
+++ introduction/052_wait_group/main.go
@@ -0,0 +1,62 @@
+/* =====================================================================
+ * -- Name ------ : Wait Group
+ * -- Date ------ : Jun 18, 2020
+ * -- Author ---- : Carlos Mendez
+ * -- Description :
+	● In addition to the main goroutine, launch two additional goroutines
+		○ Each additional goroutine should print something out
+	● Use waitgroups to make sure each goroutine finishes before your program exists
+ ===================================================================== */
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	wg.Add(2)
+	max_number := 100
+	fmt.Println("OS\t\t", runtime.GOOS)
+	fmt.Println("ARCH\t\t", runtime.GOARCH)
+
+	fmt.Println("CPUs\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines\t", runtime.NumGoroutine())
+
+	go printEvenNumbers(max_number)
+	go printOddNumbers(max_number)
+	printAllNumbers(max_number)
+
+	fmt.Println("CPUs\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines\t", runtime.NumGoroutine())
+
+	wg.Wait()
+}
+
+func printEvenNumbers(mn int) {
+	for i := 0; i < mn; i++ {
+		if i%2 != 0 {
+			fmt.Println("I'm a EVEN number: ", i)
+		}
+	}
+	wg.Done()
+}
+
+func printOddNumbers(mn int) {
+	for i := 0; i < mn; i++ {
+		if i%2 == 0 {
+			fmt.Println("I'm a ODD number: ", i)
+		}
+	}
+	wg.Done()
+}
+
+func printAllNumbers(mn int) {
+	for i := 0; i < mn; i++ {
+		fmt.Println("I'm NUMBER: ", i)
+	}
+}
