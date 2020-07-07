aa56000c03ffe8710376685e65fc9e0c2d478451
Carlos Mendez <carlos@hadaelectronics.com>
2020-07-07 17:25:00 -0500

    adding readme for same but squeared values

---
+++ Algorithms-Go/003_same_but_squared/algorithm/main.go
@@ -0,0 +1,8 @@
+// Package algorithm contains different algorithms
+package algorithm
+
+// SameButSquared compare two slices and creck if they are the same but squared
+// Create a function called "SameButSquared"
+func SameButSquared(x1 []int, x2 []int) bool {
+	return false
+}
---
+++ Algorithms-Go/003_same_but_squared/algorithm/main_test.go
@@ -0,0 +1,21 @@
+package algorithm
+
+// func TestSameButSquared(t *testing.T) {
+// 	test1 := SameButSquared([]int{1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13})
+
+// 	if test1 != 7 {
+// 		t.Error("Got: ", test1, "Expected: ", 7)
+// 	}
+// }
+
+// func BenchmarkSameButSquared(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		SameButSquared([]int{1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13})
+// 	}
+// }
+
+// func ExampleSameButSquared() {
+// 	fmt.Println(SameButSquared([]int{1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13}))
+// 	// Output:
+// 	// 7
+// }
