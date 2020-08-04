0f26e61d3bbfe34c45e84db0cbbd04c68b7bf6e2
Carlos Mendez <carlos@hadaelectronics.com>
2020-08-04 16:20:56 -0500

    improving consecutive chars

--- Algorithms-Go/027_remove_consecutive_char/algorithm/main.go
+++ Algorithms-Go/027_remove_consecutive_char/algorithm/main.go
@@ -1,8 +1,6 @@
 // Package algorithm contains different algorithms
 package algorithm
 
-import "fmt"
-
 // RemoveConsecutiveChar remove all consecutive chars
 func RemoveConsecutiveChar(str string, k int) string {
 	s := 1
@@ -11,26 +9,20 @@ func RemoveConsecutiveChar(str string, k int) string {
 	var result string
 
 	for len(str) > f {
-		fmt.Println(f, s, str)
 		if string(str[f]) != string(str[s]) {
 			f++
 			count = 0
 		} else {
 			count++
 			s++
-			if s >= len(str)-1 {
+			if s >= len(str) {
 				f++
 				s = f + 1
-				fmt.Println("===", f, len(str), count)
-				if f >= len(str)-1 {
-					if count == k {
-						result = str[:f] + str[s:len(str)]
-						return RemoveConsecutiveChar(result, k)
-					} else {
-						break
-					}
-				}
 			}
+		}
+
+		if f >= len(str) {
+			continue
 		}
 
 		if count == k {
